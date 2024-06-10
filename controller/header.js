e30adf3859eca9894c86c98707fd5e61a134ba8e
SavioVitorAlves <saviovitor2@gmail.com>
2024-06-10 00:32:38 -0300

    docs: envio de email

--- controller/header.js
+++ controller/header.js
@@ -1,11 +1,25 @@
 const button = document.querySelector("button");
 const modal = document.getElementById('modal');
-const buttonClose = document.querySelector('dialog button');
+const buttonSend = document.getElementById('enviar');
+const buttonCancell = document.getElementById('cancell');
 
 button.onclick = function (){
     modal.showModal();
 }
-buttonClose.onclick = function (){
+buttonSend.onclick = function (){
+    const nome = document.getElementById('username').value;
+    const email = document.getElementById('useremail').value;
+    const tell = document.getElementById('userphone').value;
+    const msg = document.getElementById('usermsg').value;
+    console.log(nome);
+    console.log(email);
+    console.log(tell);
+    console.log(msg);
+
+   
     modal.close();
-    alert("Sua Mensagem Foi Enviada! Obrigado pelo contado, em brave entraremos em contato com você pelo nomero adicionado!");
-} 
+    alert("Sua Mensagem Foi Enviada! Obrigado pelo contado, em brave entraremos em contato com você pelo numero adicionado!");
+}
+buttonCancell.onclick = function (){
+    modal.close();
+}
